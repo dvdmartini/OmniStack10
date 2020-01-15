b5d4cfa4830a57f2906438a600a529a86f0c4885
David <dvdmartini@gmail.com>
2020-01-15 14:50:16 -0300

    Commit initial

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+const app = express();
+
+mongoose.connect('mongodb+srv://dvdmartini:dvs270919@cluster0-act41.mongodb.net/week10?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333); //porta de entrada do localHost
---
+++ backend/src/routes.js
@@ -0,0 +1,44 @@
+const { Router } = require('express');
+const axios = require('axios');
+const routes = Router();
+const Dev = require('./models/Dev')
+
+//principais metodos HTTP usados para rota : get,post,put, delete;
+
+
+// Tipos de parametros:
+
+// Query Params: request.query (filtros,ordenação,peginação,...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: Dados:  request.body (Dados para criação ou alteração de um registro)
+
+
+//MongoDB (não relacionado)
+routes.post('/devs', async (request,response) =>{
+    const { github_username, techs, latitute, longitude } = request.body;
+
+    const apiResponse =  await axios.get(`https://api.github.com/users/${github_username}`);
+    
+    const { name = login, avatar_url, bio} = apiResponse.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+    
+    const location = {
+        type:'Point',
+        coordinates: [longitude, latitute],
+
+    }
+        
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+    });
+    
+    return response.json(dev);
+});
+
+module.exports = routes;
