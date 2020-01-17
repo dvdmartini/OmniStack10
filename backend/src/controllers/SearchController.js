27f07c87a0806d97bac8989125acd87e09742f26
David <dvdmartini@gmail.com>
2020-01-16 21:55:13 -0300

    Commit aula 2

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,28 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(request, response) {
+    //busca dos devs num raio de 10 km e por tecnologias
+    const { latitude, longitude, techs } = request.query;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+
+    return response.json({ devs });
+  },
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
-const axios = require('axios');
+
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 const routes = Router();
-const Dev = require('./models/Dev');
 
 //principais metodos HTTP usados para rota : get,post,put, delete;
 
@@ -12,32 +13,10 @@ const Dev = require('./models/Dev');
 // Body: Dados:  request.body (Dados para criação ou alteração de um registro)
 
 //MongoDB (não relacionado)
-routes.post('/devs', async (request, response) => {
-  const { github_username, techs, latitute, longitude } = request.body;
 
-  const apiResponse = await axios.get(
-    `https://api.github.com/users/${github_username}`
-  );
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-  const { name = login, avatar_url, bio } = apiResponse.data;
-
-  const techsArray = techs.split(',').map(tech => tech.trim());
-
-  const location = {
-    type: 'Point',
-    coordinates: [longitude, latitute],
-  };
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray,
-    location,
-  });
-
-  return response.json(dev);
-});
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
