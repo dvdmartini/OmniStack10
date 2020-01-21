329c3d8cc99b06f0594e3be4cf29f615ca4335a4
David <dvdmartini@gmail.com>
2020-01-20 21:06:43 -0300

    Commit inicial React-Web

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//Componente
+//Estado
+//Propriedade
+
+function App() {
+  return (
+  <h1>Salve Zê</h1>
+  );
+}
+
+export default App;
---
+++ web/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//JSX (JavaScript + HTML)
+ReactDOM.render(<App />, document.getElementById('root'));
