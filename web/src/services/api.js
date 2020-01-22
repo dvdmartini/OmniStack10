aaeeb1c789237dd3470ffae98344714afe4ebfdb
David <dvdmartini@gmail.com>
2020-01-22 08:32:43 -0300

    ajuste do ip de acesso.

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+  baseURL: "http://192.168.8.36.nip.io:3333" //localhost:3333
 });
 
 export default api;
