5cde1482d6a5e77ca8428d6dbfb70c74b86a061b
David <dvdmartini@gmail.com>
2020-01-23 21:56:32 -0300

    commit aula 4

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
-import {  StyleSheet,  Image,  View,  Text,  TextInput,  TouchableOpacity} from "react-native";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
-import {  requestPermissionsAsync,  getCurrentPositionAsync} from "expo-location";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
+import api from "../services/api";
 
 import { MaterialIcons } from "@expo/vector-icons";
 function Main({ navigation }) {
+  const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null);
+  const [techs, setTechs] = useState('');
 
   useEffect(() => {
     async function loadInitialPosition() {
@@ -28,35 +41,63 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get("/search", {
+      params: {
+        latitude,
+        longitude,
+        techs
+      }
+    });
+
+    setDevs(response.data.devs);
+  }
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region);
+  }
+
   if (!currentRegion) {
     return null;
   }
   return (
     <>
-      <MapView initialRegion={currentRegion} style={styles.map}>
-        <Marker coordinate={{ latitude: -29.1546836, longitude: -51.1899153 }}>
-          <Image
-            style={styles.avatar}
-            source={{
-              uri: "https://avatars2.githubusercontent.com/u/49644607?s=460&v=4"
-            }}
-          />
-          <Callout
-            onPress={() => {
-              navigation.navigate("Profile", { github_username: "dvdmartini" });
+      <MapView
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion}
+        style={styles.map}
+      >
+        {devs.map(dev => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              latitude: dev.location.coordinates[1],
+              longitude: dev.location.coordinates[0]
             }}
           >
-            <View style={styles.callout}>
-              <Text style={styles.devName}>David Martini</Text>
-              <Text style={styles.devBio}>
-                Estudande de Ciências da Computação
-              </Text>
-              <Text style={styles.devTechs}>
-                ReactJS, React Native, Node.js
-              </Text>
-            </View>
-          </Callout>
-        </Marker>
+            <Image
+              style={styles.avatar}
+              source={{
+                uri: dev.avatar_url
+              }}
+            />
+            <Callout
+              onPress={() => {
+                navigation.navigate("Profile", {
+                  github_username: dev.github_username
+                });
+              }}
+            >
+              <View style={styles.callout}>
+                <Text style={styles.devName}>{dev.name}</Text>
+                <Text style={styles.devBio}>{dev.bio}</Text>
+                <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
       <View style={styles.searchForm}>
         <TextInput
@@ -65,9 +106,11 @@ function Main({ navigation }) {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
         />
 
-        <TouchableOpacity onPress={() => {}} style={styles.loadButtom}>
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButtom}>
           <MaterialIcons name="my-location" size={20} color="#FFF" />
         </TouchableOpacity>
       </View>
@@ -101,41 +144,39 @@ const styles = StyleSheet.create({
     marginTop: 5
   },
   searchForm: {
-      position: 'absolute',
-      top: 20,
-      left: 20,
-      right: 20,
-      zIndex: 5,
-      display: 'flex',
-      flexDirection: 'row'
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    display: "flex",
+    flexDirection: "row"
   },
-  searchInput:{
-      flex: 1,
-      height: 50,
-      backgroundColor: '#FFF',
-      color: '#333',
-      borderRadius: 25,
-      paddingHorizontal: 20,
-      fontSize: 16,
-      shadowColor: '#000',
-      shadowOpacity: 0.2,
-      shadowOffset: {
-          width: 4,
-          height: 4,
-      },
-      elevation: 2,
-      
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#FFF",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
   },
   loadButtom: {
-      width: 50,
-      height: 50,
-      backgroundColor: '#8E4Dff',
-      borderRadius: 25,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginLeft:15,
-  },
-
+    width: 50,
+    height: 50,
+    backgroundColor: "#8E4Dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
+  }
 });
 
 export default Main;
